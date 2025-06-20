a59339b660c0e1dec71b3d544584b7a26b5939d8
nicolynk03 <kwoknicolyn@gmail.com>
2025-06-20 15:26:12 +1000

    sidebar can be closed by clicking elsewhere outside and added to about page

--- JS/script.js
+++ JS/script.js
@@ -5,3 +5,18 @@ const navbarNav = document.querySelector('.navbar-nav');
 document.querySelector('#hamburger-menu').onclick = () => {
     navbarNav.classList.toggle('active');
 };
+
+
+
+// close side menu (clicking outside of the sidebar)
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
+
+
+
+
